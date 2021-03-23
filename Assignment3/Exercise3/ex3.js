32704b06b60f79fc992beab2ba77476ef8e76cf5
Maheshwerie Samaroo <samaroomaheshwerie@gmail.com>
2021-03-23 11:37:31 -0400

    Adding files
    Added css and js files

---
+++ Assignment3/Exercise3/ex3.js
@@ -0,0 +1,39 @@
+function myFunction() { 
+      var x = document.getElementById("myText").value; 
+      var lettergrade;
+      if(x <= 39){
+      lettergrade = "Your grade is a F.";
+  	  }
+  	  	else if(x <= 49 ){
+  	  		lettergrade = "Your grade is an E.";
+  	  	}
+  	  	else if(x <= 54 ){
+  	  		lettergrade = "Your grade is a D.";
+  	  	}
+  	  	else if(x <= 59 ){
+  	  		lettergrade = "Your grade is a D+.";
+  	  	}
+  	  	else if(x <= 64 ){
+  	  		lettergrade = "Your grade is a C.";
+  	  	}
+  	  	else if(x <= 69 ){
+  	  		lettergrade = "Your grade is a C+.";
+  	  	}
+  	  	else if(x <= 74 ){
+  	  		lettergrade = "Your grade is a B.";
+  	  	}
+  	  	else if(x <= 79 ){
+  	  		lettergrade = "Your grade is a B+.";
+  	  	}
+  	  	else if(x <= 89 ){
+  	  		lettergrade = "Your grade is a A.";
+  	  	}
+  	  	else if(x <= 100 ){
+  	  		lettergrade = "Your grade is a A+.";
+  	  	}
+
+
+  	  document.getElementById("demo").innerHTML = lettergrade;
+  	 }
+
+
